6dab85740c94940f2bc8769169a810a4d7a60ba1
kangsuenghyun <parandl@naver.com>
2021-08-05 14:41:22 +0900

    저장완료 전까지

--- item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -123,21 +124,78 @@ public class BasicItemController {
      */
 
     //실제 상품 등록 로직
-   @PostMapping("/add")
-    public String addItemV4(Item item) {
-        itemRepository.save(item);
-        return "basic/item";
-    }
+//    @PostMapping("/add")
+//    public String addItemV4(Item item) {
+//        itemRepository.save(item);
+//        return "basic/item";
+//    }
 
     //근데 버전이 모두 종료되기는 했는데 난 솔직히 왜 이렇게 까지 생략을 해가면서 버전업을 해야되는건지 모르겠음
     //내가볼때 김영한도 그냥 방법의 하나로 가르쳐주는 것 뿐이지 이게 크게 중요하진 않은것 같음. 그거 몇줄이라고
     //그냥 full code로 갈기는게 나은것 같음. 의미도 제일 잘 와닿고
 
+    // 지금 addItemV1~addItemV4 이 부분은 문제가 생겨서 PRG Post/Redirect/Get 방식으로 해결해보자
+
+//    @PostMapping("/add")
+//    public String addItemvV5(Item item) {
+//        itemRepository.save(item);
+//        return "redirect:/basic/items/" + item.getId();
+//
+//        // 아니 이게 돼? 되네 ;; 이건 "redirect:/basic/items/{itemId}"; 랑 다른게 지금 여기에선 get으로 가져와야 돼고 저기서는 애초에 itemId로 받아온거니깐 그 차이인듯
+//
+//        //        Long itemId = item.getId();
+////        return "redirect:/basic/items/{itemId}";
+//        // 이건 왜 안되니ㅑ 또?
+//    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+    }
+
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
-        model.addAttribute("item",item);
+        model.addAttribute("item", item);
         return "basic/editForm";
     }
+
+    /*
+    -basic/editForm에서 저장을 누르면 밑에 post방식이 호출돼야하는데 이게 상식적으로 어떻게 되지? 싶었는데 여기도 th:action 하고 action field에 값이 없네
+    -그러면 현재 위치한 url로 post방식으로 호출이 될테니깐 밑에 api 가 호출되겠지
+     */
+//    @PostMapping("/{itemId}/edit")
+//    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+//        itemRepository.update(itemId, item);  //처음에는 이 로직을 여기서 짜버림. ㅋㅋ 근데 알고보니깐 repository에 이런 메소드가 이미 정의가 되어있었넹;ㅎㅎ
+//        return "basic/items";
+//
+// }
+        
+        /*
+        맨처음에 redirect 해서 띠용 싶었는데 이게 어쩔수가 없는 구조네
+        왜냐면 생각을 해보면 수정 버튼을 눌르면 이제 수정된 정보가 딱 item 상세 로 떠야 되는데 여기서 만약 그냥 basic/item html 파일을 호출한다? 그러면 model 에 itemid를 넣어서 호출할 수가 없잖아
+        그냥 완전 정적 파일을 호출하는것 그 이상 그 이하도 아니지
+        그럼 동적으로 id를 넣어서 호출을하려면? 상세 페이지를 띄우는 mapping uri로 redirect하면 원큐에 해결되지
+
+        물론 근데 방법은 있을 것 같기도 함. 정적 html 에 모델을 우겨넣을수만 있으면 되는걸테니깐
+
+        근데 왜 그냥 "basic/html" 단순 html 파일만 return해줬는데 동작하는거냐?
+        유일한 가능세계는 스프링에서는 @ModelAttribute 가 있으면 자동으로 model에 addAttribute를 시켜준다는 건데 이것도 위를 참고하면
+        model이 넘어올 경우나 그런거고
+
+         */
+
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    //일단 redirect version에서도 되기는 함
+    //이 부분은 확실하게 알려면 일단 다 해보고 강의를 쫙 들으면서 체크해보면 될듯듯
 }
